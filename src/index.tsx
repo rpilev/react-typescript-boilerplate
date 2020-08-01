800a71186236ab4f4b1351ee549850c2e4cd3f9d
rpilev <raulshpilev@gmail.com>
2020-08-01 22:01:52 +0300

    Add lint and lint-fix scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
-import React from 'react'import ReactDom from 'react-dom'import { App } from './app'const render = (): void => (  ReactDom.render(    <App />,    document.getElementById('root'),  ))render()
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import { App } from './app'
+
+
+const render = (): void => (
+  ReactDom.render(
+    <App />,
+    document.getElementById('root'),
+  )
+)
+
+render()
