33b78a96db049a95787dac3d32bd88612e86d916
rpilev <raulshpilev@gmail.com>
2020-08-01 17:53:55 +0300

    Create simple react typescript boilerplate

---
+++ src/app.tsx
@@ -0,0 +1,7 @@
+import React, {ReactElement} from 'react'
+
+export const App = (): ReactElement => (
+  <div>
+    Test
+  </div>
+)
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React, {ReactElement, ReactInstance} from 'react'
+import ReactDom from 'react-dom'
+
+import { App } from './app'
+
+
+const render = (): void => (
+  ReactDom.render(
+    <App/>,
+    document.getElementById("root")
+  )
+)
+
+render()
